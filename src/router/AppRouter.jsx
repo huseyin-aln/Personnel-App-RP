f6d4bbf3b36aca4f6645a8c7ae164d4fac3c4eaa
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-10-19 14:39:00 +0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Home from "../pages/Home";
+import NavBar from "../components/Navbar";
+import DeparmentDetail from "../pages/DepartmentDetail";
+import PersonalCreate from "../pages/PersonalCreate";
+import PrivateRouter from "./PrivateRouter";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<PrivateRouter />}>
+          <Route path="" element={<Home />} />
+        </Route>
+        <Route path="/detail/:str" element={<PrivateRouter />}>
+          <Route path="" element={<DeparmentDetail />} />
+        </Route>
+        <Route path="/create-personal" element={<PrivateRouter />}>
+          <Route path="" element={<PersonalCreate />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
